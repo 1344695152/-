edda1279f01f43b34ac5bdc30c547c6c03df514c
1344695152 <1344695152@qq.com>
2020-08-27 14:36:40 +0800

    Update Program.cs

--- HashCodeTest/Program.cs
+++ HashCodeTest/Program.cs
@@ -6,27 +6,35 @@ namespace HashCodeTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             ren r1 = new ren();
-            ren r2 = new ren();
-            r1.age = 1;
-            r1.name = "1";
-            r2.age = 1;
-            r2.name = "1";
+            r1.age = 2;
+            r1.name = "的撒赫迪拉杀伤距离";
             Console.WriteLine($"r1的哈希值{r1.GetHashCode()}");
-            Console.WriteLine($"r2的哈希值{r2.GetHashCode()}");
 
+            ren r2 = new ren();
+            r2.age = 2;
+            r2.name = "的撒赫迪拉杀伤距离";
+            Console.WriteLine($"r2的哈希值{r2.GetHashCode()}");
 
             wu r3 = new wu();
             r3.age = 1;
-            r3.name = "1";
+            r3.name = "的撒赫迪拉杀伤距离";
+            Console.WriteLine($"r3的哈希值{r3.GetHashCode()}");
 
             wu r4 = new wu();
             r4.age = 1;
-            r4.name = "1";
-          
-            Console.WriteLine($"r3的哈希值{r3.GetHashCode()}");
+            r4.name = "的撒赫迪拉杀伤距离";
             Console.WriteLine($"r4的哈希值{r4.GetHashCode()}");
+
+            string test1 = JsonConvert.SerializeObject(r3);
+            Console.WriteLine($"r3json的哈希值{test1.GetHashCode()}");
+
+            string test2 = JsonConvert.SerializeObject(r4);
+            Console.WriteLine($"r4json的哈希值{test2.GetHashCode()}");
+
+            r4.age = 0;
+            string test3 = JsonConvert.SerializeObject(r4);
+            Console.WriteLine($"r4改值后json的哈希值{test3.GetHashCode()}");
             Console.ReadKey();
         }
     }
